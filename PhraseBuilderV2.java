ac5fbf4975d5667dae9cede68fac31f7111f268a
Mr-William <william.gibbs.4@outlook.com>
2021-10-13 19:38:26 -0500

    Initial Publish
    First upload

---
+++ PhraseBuilderV2.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phrasebuilderv2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author William Gibbs
+ */
+public class PhraseBuilderV2 extends Application 
+{
+
+    @Override
+    public void start(Stage stage) throws Exception 
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Phrase Builder 2.0");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        try
+        {
+        launch(args);
+        }
+        catch (Throwable t)
+        {
+            JOptionPane.showMessageDialog(null, t.getClass().getSimpleName() 
+                                          + ": " + t.getMessage());
+            
+            throw t;
+        }
+    }
+    
+}
